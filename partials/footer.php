23aa40c82d4232ca710ea94642fc73e1908e5c57
!NG. LUIS T😎NCEL <rdsdevs@gmail.com>
2025-09-24 17:26:28 -0500

    Se agrego el año de forma dinamica y el copyright symbol en el footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,7 @@
 </main>
 <div class="position-relative">
   <div class="position-absolute top-100 start-50 translate-middle">
-    Todos los derechos reservados. By <b class="whatsapp">rdsDev</b>
+    <?=date('Y')?> Todos los derechos reservados. By <b class="whatsapp">rdsDev</b>
     <!-- <p class="text-center"></p> -->
   </div>
 </div>
